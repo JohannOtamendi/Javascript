e24eec7ea809ba195dcdabe412e3e6ae238d72d2
JohannOtamendi <johanom718@gmail.com>
2022-08-29 21:45:11 -0500

    primeros pasos Js

---
+++ ejercicioClase1.js
@@ -0,0 +1,110 @@
+console.log('Hello World')
+
+let deporte = "Atletismo";
+
+// // procurar que las variables sean lo mas declarativas posible
+// let saludo = "Buenos dias";
+// let pais = "México";
+// let edad = "53";
+
+// // camelcase
+// let nombreDeMiPerro = "foxy";
+// let animalVerde = "Iguana"
+
+// Tipos de datos
+// let valorbooleano ="true"
+// let objecto = {
+//     propiedad:"perro"
+//     valor: foxy
+// }
+// // string
+// let a = "Let's learn Javascript";
+
+// // number
+// let a = "12"
+// let b = "32.43"
+// let c = 
+
+// console.log(7/0)    
+// console.log (-7/0)
+// console.log(Math.sqr(-1))
+
+// console.log("hello world" /2)
+
+// let a;
+// let b= "hello world"
+
+// a =null 
+
+// Numbers
+// typeof traduce Js a nosotros
+// typeof 10;	// "number"
+// typeof 12.5;	// "number"
+// typeof 2.5e+6;	// "number"
+// typeof Infinity;	// "number"
+// typeof NaN;	// "number"
+
+// // Strings
+// typeof ' ';	// "string" - Espacios en blanco cuentan como caracteres
+// typeof 'Hello World';	// "string"
+// typeof '12';	// "string" - Números dentro de comillas son strings
+
+// // Booleans
+// typeof true;	// "boolean"
+// typeof false;	// "boolean"
+
+// // Undefined
+// typeof undefined;	// "undefined"
+
+// var name;
+// typeof name; // "undefined" - Aún no se asigna un valor
+
+// // Null
+// typeof Null;	// "object"
+
+// // console.log (string (123))//explicita
+// // 123 +''//implicita
+// 1- '1'
+// 2* '2'
+
+// Boolean 
+
+let a =5;
+let b =10;
+let c =15;
+
+// a + b;  // 15
+
+// b - a;  // 5
+
+// a * c;  // 75
+
+// c / a;  // 3
+
+// b % a;  // 0
+
+// Javascript lee de izquierda a derecha y de arriba hacia abajo
+
+// a + b * c
+
+// (a + b) * c 
+
+// b / a * c 
+
+// b / (a * c) 
+
+// a - b + c
+
+// a - (b+c)
+
+(3 + b) * c / a * 2; //78
+
+((a + b * c) / 5) * 2; //62
+
+b / a + 2 * c; //32
+
+(a + b + c / c) * a; //80
+
+((3 * a) / c) + a + b + c;  //31
+
+a - (b + c) * a / 1; //-125
